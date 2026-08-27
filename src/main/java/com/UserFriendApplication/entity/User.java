fba455babfd244486ba5f5acbc30aae09a29203a
omkar dubbe <dubbeom@gmail.com>
2026-08-27 13:11:37 +0530

    add new feilds "UserName"

--- src/main/java/com/UserFriendApplication/entity/User.java
+++ src/main/java/com/UserFriendApplication/entity/User.java
@@ -18,6 +18,9 @@ public class User {
     @Column(nullable = false)
     private String name;
 
+    @Column(name = "username", nullable = false)
+    private String username;
+
     @Column(nullable = false, unique = true)
     private String email;
 
